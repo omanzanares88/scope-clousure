ed5eb60713c8c381db325c8b9d7e36d4068414cc
Oz <cuevas_osk@hotmail.com>
2025-01-04 14:04:48 -0600

    practica Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+//no puedes ejecutar primero el valor mostrado
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+
+//ejemplo en una funcion, el llamado de la funcion no se ejecuta y se encuentra al inicio
+
+nameOfDog2();
+
+function nameOfDog2() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
